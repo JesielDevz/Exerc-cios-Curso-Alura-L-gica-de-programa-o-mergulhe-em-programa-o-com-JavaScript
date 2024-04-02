7d755976ddad0e266ce667480835c45fc096201a
JesielDevz <165445228+JesielDevz@users.noreply.github.com>
2024-04-02 10:35:56 -0300

    Add files via upload
    Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.

---
+++ exercicio009.js
@@ -0,0 +1,10 @@
+//Crie um programa de contagem progressiva. 
+//Peça um número e conte de 0 até esse número,
+//usando um loop while no console do navegador.
+
+contador = 0;
+numeroEscolhido = parseInt(prompt('Digite um número positivo(Maior que zero)'))
+while (contador <= numeroEscolhido) {
+    alert(contador);
+    contador++;
+}
