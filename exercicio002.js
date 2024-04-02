e729d0a502aa8e7d8f63c2aa5ccb3f34dd38e700
JesielDevz <165445228+JesielDevz@users.noreply.github.com>
2024-04-02 00:48:26 -0300

    Create exercicio002.js
    2 - Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.

---
+++ exercicio002.js
@@ -0,0 +1,10 @@
+//2 - Verifique se um número digitado pelo usuário é positivo ou negativo.
+//Mostre um alerta informando.
+
+let numeroDigitado = prompt('Digite um número qualquer');
+
+if (numeroDigitado >= 0 ) {
+    alert('Este número é positivo!');
+} else {
+    alert('Este número é negativo!');
+}
