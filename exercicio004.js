2eba921f1801824069f88ca4327bb45f73355216
JesielDevz <165445228+JesielDevz@users.noreply.github.com>
2024-04-02 00:53:43 -0300

    Add files via upload

---
+++ exercicio004.js
@@ -0,0 +1,5 @@
+//4 - Crie uma mensagem que informa o usuário sobre o saldo da conta,
+//usando uma template string para incluir o valor do saldo.
+
+let saldoConta = prompt('Digite o valor de depósito');
+alert(`O valor do seu saldo é de R$${saldoConta}.`);
