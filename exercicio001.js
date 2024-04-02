9b76e5e81cf457cebe894d753d411f9380a72200
JesielDevz <165445228+JesielDevz@users.noreply.github.com>
2024-04-02 00:47:42 -0300

    exercicio001.js
    1 - Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!".
    Caso contrário, mostre "Boa semana!".

--- exercicio001.js
+++ exercicio001.js
@@ -1,1 +1,13 @@
+//1 - Pergunte ao usuário qual é o dia da semana. 
+//Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!".
+//Caso contrário, mostre "Boa semana!".
 
+let diaSemana = prompt('Que dia da semana é hoje?');
+
+if (diaSemana == 'Sábado') { 
+    alert('Bom fim de semana!');
+} else if (diaSemana == 'Domingo') {
+    alert('Bom fim de semana!');
+} else {
+    alert('Boa semana!');
+}
