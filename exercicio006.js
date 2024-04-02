b2bc3d6f92732c45b1f52256eac268850b3de554
Jesiel Silva <165445228+JesielDevz@users.noreply.github.com>
2024-04-02 10:43:15 -0300

    exercicio006.js

--- exercicio006.js
+++ exercicio006.js
@@ -1,7 +1,7 @@
 //Crie um contador que comece em 1 e vá até 10 usando um loop while.
 //Mostre cada número.
 
-contador = 1;
+let contador = 1;
 while (contador <= 10 ) {
     alert(contador);
     contador++;
