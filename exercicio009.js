f40457ba6d1e804187e3ada1f1688be527adb4c2
Jesiel Silva <165445228+JesielDevz@users.noreply.github.com>
2024-04-02 10:43:52 -0300

    exercicio009.js

--- exercicio009.js
+++ exercicio009.js
@@ -2,7 +2,7 @@
 //Peça um número e conte de 0 até esse número,
 //usando um loop while no console do navegador.
 
-contador = 0;
+let contador = 0;
 numeroEscolhido = parseInt(prompt('Digite um número positivo(Maior que zero)'))
 while (contador <= numeroEscolhido) {
     alert(contador);
